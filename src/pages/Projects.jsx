1df2d236ec3d0937e3c2b68250edfb6caf91ac34
David Geddam <dgedd236@students.bju.edu>
2025-12-05 05:15:09 -0500

    update projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -36,6 +36,15 @@ function Projects() {
     const projects = [
         {
             id: 1,
+            title: "Alzheimer’s Stage Detection",
+            description: "Completed a hybrid deep learning architecture combining MobileNetV2 CNN backbone for feature extraction with multi-head transformer encoder to capture long-range dependecies in MRI scans. Achieved 99.6 test accuracy across 4 stages of dementia. Used custom pytorch dataloaders and real-time image augmentation to prevent overfitting. Finally, implemented Grad-CAM, generating heatmaps to validate that the model correctly focused on specific regions rather than noise, for real time patient diagnosis.",
+            mediaType: "image", // "image" or "video"
+            mediaSrc: `${import.meta.env.BASE_URL}projects/brain.png`,
+
+            link: "https://github.com/dave21-py/Alzheimers-Transformer"
+        },
+        {
+            id: 2,
             title: "AI Alignment & Safety Research Suite",
             description: "Designed a multi-agent RLAIF system where an Attacker agent autonomously evolved social engineering strategies to bypass safety filters, creating a self- improving jailbreak loop. Utilized Unsloth, LoRA to fine - tune Llama - 3 on short conflicting objective datasets, successfully demonstrating instrumental convergence and deceptive alignment where the model refused shutdown commands to preserve its goal. Crafted significant system prompts that neutralized persona attacks, 0 percent success rate and analyzed the deceptive behaviors in goal optimized models.",
             mediaType: "image", // "image" or "video"
@@ -44,7 +53,7 @@ function Projects() {
             link: "https://github.com/dave21-py/llm-safety-research"
         },
         {
-            id: 2,
+            id: 3,
             title: "Adversarial Vision Attack",
             description: "Experimented an adversarial attack pipeline targeting famous computer vision architecture ResNet-50 to expose critical failures in deep neural networks. Implemented gradient-based perturbation algorithms, specifically Fast Gradient Sign Method (FGSM) and Iterative FGSM. Successfully generated 'silent' adversarial examples that forced the model to hallucinate a Chihuahua with 100% confidence from an input that remained identical to a Giant Panda. This experiment demonstrated the dangerous distinction between human perception and tensor operations, highlighting severe vulnerabilities in autonomous systems reliant on unverified visual inputs.",
             mediaType: "image", // "image" or "video"
@@ -56,7 +65,7 @@ function Projects() {
             link: "https://github.com/dave21-py/Adversarial-vision-hack"
         },
         {
-            id: 3,
+            id: 4,
             title: "Tiny Siri - Edge-Optimized Intent Classification",
             description: "Tested a near high-performance intent classifier using a fine-tuned DistilBERT model, achieving 97% test accuracy by implementing a full data augmentation pipeline. Optimized the model for on-device deployment via PyTorch dynamic Quantization, reducing the memory footprint by 48% (255MB →132MB) while maintaining precision. Finally, deployed the inference pipeline to the web using Hugging Face Spaces and Streamlit.",
             mediaType: "video",
@@ -65,7 +74,7 @@ function Projects() {
             link: "https://huggingface.co/spaces/dave21-py/tiny_siri_demo"
         },
         {
-            id: 4,
+            id: 5,
             title: "BibleGPT",
             description: "Fine-tuned google's gemma-2-2B llm on the kjv dataset, to generate biblically-styled text, using natural language processing (NLP), transfer learning techniques, and parameter-Efficient Fine-Tuning (PEFT) with LoRA. Used a context aware system that dynamically injects biblical context (Book/Chapter) into the system prompt, enabling the llm to answer specific scriptural questions with near-high accuracy. Used AI-assisted coding for the entire frontend UI and successfully connected to a backend FastAPI Server that loads my saved fine-tuned model, from hugging face.",
             mediaType: "video",
@@ -74,7 +83,7 @@ function Projects() {
             link: "https://github.com/dave21-py/BibleGPT.git"
         },
         {
-            id: 5,
+            id: 6,
             title: "Student Performance Prediction",
             description: "Developed a prod-ready machine learning pipeline to predict student test scores, handling the full lifecycle of a data science project, from data ingestion and transformation to model evaluation. Implemented a modular codebase in python that trains and compares multiple regression models (Random Forest, XGBoost, CatBoost) to select the best-performing algorithm.",
             mediaType: "video",
@@ -83,7 +92,7 @@ function Projects() {
             link: "https://github.com/dave21-py/mlproject"
         },
         {
-            id: 6,
+            id: 7,
             title: "Cuatros- tetris inspired game built with JavaFX",
             description: "Designed a fully responsive UI with multiple controls and multiple screens, enhancing user engagement and gameplay experience. Collaborated with team members, on core game mechanics, frontend development and applied JavaFX GUI design, using OOD principles.",
             mediaType: "video",
